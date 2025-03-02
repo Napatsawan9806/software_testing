fcb94eef4f20946520142a10a3ac1ee7a5b9782f
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-03-02 19:28:09 +0700

    add test case

---
+++ hackerrank/tests/test_alternate_utils.py
@@ -0,0 +1,17 @@
+import unittest
+from prob.alternate_utils import alternate
+
+
+class TestAlternate(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(alternate("beabeefeab"), 5)
+        self.assertEqual(alternate("aaaa"), 0)
+        self.assertEqual(alternate("abcabc"), 4)
+
+    def test_no_valid_pairs(self):
+        self.assertEqual(alternate("x"), 0)
+        self.assertEqual(alternate("xyz"), 2)
+
+    def test_max_length(self):
+        self.assertEqual(alternate("abababab"), 8)
+        self.assertEqual(alternate("aabbccddeeffgghh"), 0)
