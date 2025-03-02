30c45d499e84fd9a3a4bba17d31b48f52286e632
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-03-02 19:10:19 +0700

    add test case

---
+++ hackerrank/tests/test_alternate_char_utils.py
@@ -0,0 +1,11 @@
+import unittest
+from prob.alternate_char_utils import alternatingCharacters
+
+
+class TestAlternatingCharacters(unittest.TestCase):
+    def test_cases(self):
+        self.assertEqual(alternatingCharacters("AABAAB"), 2)
+        self.assertEqual(alternatingCharacters("AAAA"), 3)
+        self.assertEqual(alternatingCharacters("BBBBB"), 4)
+        self.assertEqual(alternatingCharacters("ABABABAB"), 0)
+        self.assertEqual(alternatingCharacters("AAABBB"), 4)
